d508f49798011fb0978a76f18eb490d4aafeac64
pradnya <pradnyahegde@gmail.com>
2019-10-05 13:46:45 -0400

    Added high level psudocode to file

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//Load PokeAPI , Giphy and Plotly API
+//Configure Firebase database
+//Initialize the firebase app
+//Get database reference
+
+//Store all the retrieved pokemon data in firebase
+
+//when a navbar element is clicked , display images for that category
+
+//when an image is clicked retrieve the stats from PokeAPI and display on screen
+//Input the same stats to Ploytly API and generate a bar graph, display graph on screen
+
+//on exit remove all the userdata from firebase 
